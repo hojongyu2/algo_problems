1c451495037b9f3c6496c55721ad1a1e2b59a41f
hojongyu2 <yuhojo91@gmail.com>
2023-03-01 22:00:29 -0600

    top kth frequent element solution with python

--- medium/top_kth_frequent_element/top_kth_frequent_element.py
+++ medium/top_kth_frequent_element/top_kth_frequent_element.py
@@ -1,3 +1,20 @@
 class Solution:
     def topKFrequent(nums, k):
-        pass
+        # create hashmap as key value pair, where key as an inter in list, 
+        # value as amount of int appears in the list
+        # then sort out the dict as it's value to decending order, 
+        # return kth most frequent key.
+        num_dict = {}
+        for num in nums:
+            num_dict[num] = 1 + num_dict.get(num, 0)
+        # this is same thing as 
+        # for num in nums:
+        #     if num in num_dict:
+        #         num_dict[num] += 1
+        #     else:
+        #         num_dict[num] = 1
+        sorted_dict = dict(sorted(num_dict.items(), key=lambda item: item[1], reverse = True))
+        return list(sorted_dict)[:k]
+            
+
+        
